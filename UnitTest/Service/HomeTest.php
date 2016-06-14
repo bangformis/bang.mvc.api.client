11f679836ee79225f777ab1999847e006899efef
Bang <bang@cycu.org.tw>
2016-06-14 15:45:02 +0800

    新增UnitTest和调整API结构假设很多API需要介接的系统

---
+++ UnitTest/Service/HomeTest.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'auto_load.php';
+
+use Models\API\ApiName\Service;
+
+/**
+ * 字串功能測試
+ */
+class HomeTest extends PHPUnit_Framework_TestCase {
+
+    public function testTestSuccess() {
+        $service = new Service\Home();
+        $email = 'bang@bang.mvc';
+
+
+        $result1 = $service->TestSuccess($email);
+        $this->assertTrue($result1->IsSuccess);
+        $this->assertEquals($result1->Message, 'test success!');
+
+        $result2 = $service->TestUnSuccess($email);
+        $this->assertFalse($result2->IsSuccess);
+        $this->assertEquals($result2->Message, 'test unsuccess!');
+    }
+
+}
